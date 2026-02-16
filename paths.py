2f46233607c7e60309c1bb433a53c20c926e7cbd
Syed Obaidullah Hassan Chishti <obaidullahhasan@gmail.com>
2026-02-16 22:30:52 +0500

    Add cleaned data directory and update transformation pipeline
    - Introduced a new directory for cleaned data in paths.py.
    - Updated transformation_pipeline.ipynb to use the new FilePaths enum for directory paths.
    - Adjusted execution counts and outputs in the notebook for consistency.

--- paths.py
+++ paths.py
@@ -4,6 +4,7 @@ from pathlib import Path
 class FilePaths(Enum):
     RAW_DATA_DIR = "data/raw"
     PROCESSED_DATA_DIR = "data/processed"
+    CLEANED_DATA_DIR = "data/cleaned"
 
 for path in FilePaths:
     Path(path.value).mkdir(parents=True, exist_ok=True)
